89e114c44628c56a72677459c423820206d0b954
Aryan Gupta <anaryagu@gmail.com>
2026-08-29 17:22:33 +0530

    new docs changes

--- components/dock.jsx
+++ components/dock.jsx
@@ -3,8 +3,9 @@ import { Tooltip } from "react-tooltip";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-
+import useWindowStore from "../store/window.js";
 const Dock = () => {
+    const {openWindow,closeWindow,windows} = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -59,7 +60,18 @@ const Dock = () => {
         };
     }, []);
 
-    const toggleApp = (app) => {};
+    const toggleApp = (app) => {
+        if(!app.canOpen) return ;
+
+        const window = windows[app.id];
+
+        if(window.isOpen){
+            closeWindow(app.id)
+        }else{
+            openWindow(app.id)
+        }
+        console.log(windows);
+    };
 
     return (
         <section id="dock">
